dd6c6cab1335924a039320081721a461e1b6e19b
Camilo Moreno <camilomorenom@gmail.com>
2025-07-30 10:49:02 -0500

    busqueda de peliculas

--- movie/views.py
+++ movie/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Movie
+
 
 def home(request):
-    return render(request, "home.html", {'name':'Camilo Moreno'})
+    searchTerm = request.GET.get('searchMovie')
+    if searchTerm:
+            movies = Movie.objects.filter(title__icontains=searchTerm)
+    else:
+        movies = Movie.objects.all()
+    return render(request, "home.html", {'searchTerm':searchTerm, 'movies':movies})
+    #return render(request, "home.html", {'name':'Camilo Moreno'})
 
 def about(request):
-    return HttpResponse("About Page")
+    return render(request, "about.html")
--- moviereviews/urls.py
+++ moviereviews/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.urls import path
 from movie import views as movieViews
 
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,3 +27,6 @@ urlpatterns = [
     path('about/', movieViews.about),
 
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
